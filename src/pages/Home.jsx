77e233ae087e38bfcdf36e17ce7bfffe7af593fb
mahmoraess <maria.moraes6@estudante.ifms.edu.br>
2024-11-23 18:38:10 -0400

    atualização botões

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,9 +26,34 @@ if (lista.length === 0) {
     )
 }
 
+    const orderAz = () =>{
+     const ListaAux = [...lista].sort((a,b) => a.title.localeCompare(b.title));
+     setLista(ListaAux);
+}
+
+    const orderZa = () =>{
+    let ListaAux = [...lista].sort((a,b) => a.title.localeCompare(b.title));
+    ListaAux = ListaAux.reverse();
+    setLista(ListaAux);
+}
+
+const orderPriceAsc = () => {
+    const listaOrdenada = [...lista].sort((a, b) => a.price - b.price);
+    setLista(listaOrdenada);
+  };
+
+  const orderPriceDesc = () => {
+    const listaOrdenada = [...lista].sort((a, b) => b.price - a.price);
+    setLista(listaOrdenada);
+  };
+
     return (
       <>
        <h1>★Lista de Produtos★</h1>
+       <button onClick={()=> orderAz()} >Az</button>
+       <button onClick={()=> orderZa()} >Za</button>
+       <button onClick={orderPriceAsc}>Preço: Menor para Maior</button>
+      <button onClick={orderPriceDesc}>Preço: Maior para Menor</button>
        <ListarProdutos produtos={lista}/>
     </>
   );
